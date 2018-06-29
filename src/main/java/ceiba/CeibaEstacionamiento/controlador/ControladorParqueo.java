14d8076f905dc32022f70084c747cf1c2a12b2e8
Andrés Felipe García Granados - Ceiba Software <andres.granados@ceiba.com.co>
2018-06-29 17:11:16 -0500

    se habilita cors

--- src/main/java/ceiba/CeibaEstacionamiento/controlador/ControladorParqueo.java
+++ src/main/java/ceiba/CeibaEstacionamiento/controlador/ControladorParqueo.java
@@ -4,18 +4,22 @@ import java.text.SimpleDateFormat;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import ceiba.CeibaEstacionamiento.dominio.Parqueadero;
 import ceiba.CeibaEstacionamiento.dominio.Vehiculo;
 import ceiba.CeibaEstacionamiento.dominio.Vigilante;
 import ceiba.CeibaEstacionamiento.modelo.ModeloVehiculo;
 
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping("/ceiba")
 public class ControladorParqueo {
