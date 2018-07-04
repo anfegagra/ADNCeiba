8b23004025c777f037fcd48ee8235e53f553957a
Pipe <andres.granados@ceiba.com.co>
2018-07-03 23:18:43 -0500

    terminando pruebas funcionales

--- src/test/java/ceiba/CeibaEstacionamiento/dominio/funcionales/EstacionamientoTest.java
+++ src/test/java/ceiba/CeibaEstacionamiento/dominio/funcionales/EstacionamientoTest.java
@@ -4,9 +4,12 @@ import static org.junit.Assert.*;
 
 import java.util.concurrent.TimeUnit;
 
+import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -14,10 +17,34 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import ceiba.CeibaEstacionamiento.dominio.repositorio.RepositorioVehiculo;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment=WebEnvironment.DEFINED_PORT)
 public class EstacionamientoTest {
 	
+	private static final String REGISTRO_EXITOSO = "Registro exitoso!";
+	private static final String RETIRO_EXITOSO = "Retiro exitoso!";
+	
 	private static WebDriver driver = null;
+	
+	@Autowired
+	RepositorioVehiculo repositorioVehiculo;
+
+	@Before
+	public void setUp(){
+		repositorioVehiculo.deleteAll();
+	}
+	
+	@After
+	public void tearDown(){
+		repositorioVehiculo.deleteAll();
+	}
 	
 	@BeforeClass
 	public static void inicializarDriver(){
@@ -31,31 +58,86 @@ public class EstacionamientoTest {
 	}
 
 	@Test
-	public void test() throws InterruptedException {
-		//boolean result = false;
-		String resultado = "";
-		String registroExitoso = "Registro exitoso!";
-		String registroFallido = "El vehiculo ya se encuentra en el parqueadero";
+	public void registrarEntradaMoto() {	
+		
+		driver.get("http://localhost:4200/");
+				
+		WebElement inputPlaca = driver.findElement(By.id("inputPlaca"));
+		inputPlaca.sendKeys("JNH76I");
+		
+		WebElement inputCilindraje = driver.findElement(By.id("inputCilindraje"));
+		inputCilindraje.sendKeys("200");		
+		
+		WebElement botonRegistrar = driver.findElement(By.id("botonRegistrar"));
+		botonRegistrar.click();
+		
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		WebElement mensajeResp = driver.findElement(By.id("contenedorRespuesta"));
+		assertTrue(mensajeResp.getText().equals(REGISTRO_EXITOSO));		
+	}
+	
+	@Test
+	public void registrarEntradaCarro() {	
+		
+		driver.get("http://localhost:4200/");
+				
+		WebElement inputPlaca = driver.findElement(By.id("inputPlaca"));
+		inputPlaca.sendKeys("UBV603");	
+		
+		WebElement botonRegistrar = driver.findElement(By.id("botonRegistrar"));
+		botonRegistrar.click();
+		
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		WebElement mensajeResp = driver.findElement(By.id("contenedorRespuesta"));
+		assertTrue(mensajeResp.getText().equals(REGISTRO_EXITOSO));		
+	}
+	
+	@Test
+	public void registrarSalidaCarro() throws InterruptedException {	
 		
 		driver.get("http://localhost:4200/");
 		
-		WebElement placa = driver.findElement(By.id("inputPlaca"));
-		placa.sendKeys("VVV333");
+		String placa = "UBV603";
+				
+		WebElement inputPlaca = driver.findElement(By.id("inputPlaca"));
+		inputPlaca.sendKeys(placa);	
 		
-		WebElement cilindraje = driver.findElement(By.id("inputCilindraje"));
-		cilindraje.sendKeys("200");
+		WebElement botonRegistrar = driver.findElement(By.id("botonRegistrar"));
+		botonRegistrar.click();
 		
+		Thread.sleep(2000);
+		WebElement botonRetirar = driver.findElement(By.id(placa));
+		botonRetirar.click();
 		
-		WebElement registrar = driver.findElement(By.id("botonRegistrar"));
-		registrar.click();
-		
-		driver.manage().timeouts().implicitlyWait(1,TimeUnit.SECONDS);
+		Thread.sleep(2000);
 		WebElement mensajeResp = driver.findElement(By.id("contenedorRespuesta"));
-		System.out.println("RESPUESTA: "+mensajeResp.getText());
+		assertTrue(mensajeResp.getText().equals(RETIRO_EXITOSO));		
+	}
+	
+	@Test
+	public void consultarCarro() throws InterruptedException{	
 		
-		assertTrue(mensajeResp.getText().equals("Registro exitoso!"));
+		driver.get("http://localhost:4200/");
 		
+		String placa = "UBV603";
+				
+		WebElement inputPlaca = driver.findElement(By.id("inputPlaca"));
+		inputPlaca.sendKeys(placa);	
 		
+		WebElement botonRegistrar = driver.findElement(By.id("botonRegistrar"));
+		botonRegistrar.click();
+		
+		Thread.sleep(1000);
+		WebElement inputConsulta = driver.findElement(By.id("inputConsulta"));
+		inputConsulta.sendKeys(placa);
+		
+		WebElement botonConsultar = driver.findElement(By.id("botonConsultar"));
+		botonConsultar.click();
+		
+		Thread.sleep(1000);
+		WebElement resultadoPlaca = driver.findElement(By.id("labelPlaca"));
+		System.out.println("placaaaa-" + resultadoPlaca.getText());
+		assertTrue(resultadoPlaca.getText().equals("Placa: " + placa));	
 	}
 
 }
